f98f50f99cc01e041ab7fb51ef8025fe8d19cb72
FernandoAgostini28 <fernando.agostini.28@gmail.com>
2021-10-26 22:35:00 -0300

    pergunta 02

--- PerguntasFernando.sql
+++ PerguntasFernando.sql
@@ -13,4 +13,17 @@ FROM
 WHERE
     jogos.temporada = '2019'
     group by times.id, times.apelido order by sum(jogos.PTS_visitante) desc
-LIMIT 15
+LIMIT 15;
+
+-- top 5 Temporadas com Maiores Pontuações
+
+SELECT 
+    jogos.temporada,
+    SUM(jogos.PTS_casa) as Total_pontos_casa,
+    SUM(jogos.PTS_visitante) as Total_pontos_Visitantes,
+    SUM(jogos.PTS_visitante + jogos.PTS_casa) as Total_pontos 
+FROM
+    t8grupo1.jogos AS jogos	
+
+    group by jogos.temporada order by sum(jogos.PTS_casa + jogos.PTS_visitante) desc
+LIMIT 5;
